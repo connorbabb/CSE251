37e7137defa219fb65c62814b7662382fcaef5ea
connorbabb <145483698+connorbabb@users.noreply.github.com>
2024-09-30 11:10:59 -0600

    relocation commit

--- lesson_02/team/team.py
+++ lesson_02/team/team.py
@@ -61,7 +61,7 @@ if __name__ == '__main__':
     #        team_get_deck_id.py program once. You can have
     #        multiple decks if you need them
 
-    deck_id = 'ENTER ID HERE'
+    deck_id = 'fpvh7t5dpjlr'
 
     # Testing Code >>>>>
     deck = Deck(deck_id)
